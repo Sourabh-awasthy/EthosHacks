fc78dd47767d13a7403b3ef98c3d64f0ecac4558
mehulambastha <mehul.213amb@gmail.com>
2025-10-05 03:07:45 +0530

    [BACKEND] remove duplicacy

--- backend-ethos/main.go
+++ backend-ethos/main.go
@@ -58,9 +58,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
 }
