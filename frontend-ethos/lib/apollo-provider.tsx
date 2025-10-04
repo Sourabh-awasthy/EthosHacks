51a36a755375f368b784da49d116d5edb5fe1200
Sourabh-awsthy <sourabhawasthy176022@gmail.com>
2025-10-05 02:08:52 +0530

    [ApolloClient] added

---
+++ frontend-ethos/lib/apollo-provider.tsx
@@ -0,0 +1,41 @@
+// lib/apollo-provider.tsx
+
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+// This function creates a new Apollo Client instance for each request.
+function makeClient() {
+  const httpLink = new HttpLink({
+    // Replace this with your GraphQL API endpoint.
+    uri: "https://flyby-router-demo.herokuapp.com/", // Example public API
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+// The ApolloWrapper component provides the client to its children.
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
